0bbc53901ea67e95521af0ce7bc664216f9b1b55
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-07 21:47:22 +0200

    added the game screen (#41)

---
+++ src/Scenes/uSceneGame.pas
@@ -0,0 +1,74 @@
+unit uSceneGame;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Graphics,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Dialogs,
+  FMX.StdCtrls,
+  _SceneAncestor, FMX.Controls.Presentation;
+
+type
+  TSceneGame = class(T__SceneAncestor)
+    Label1: TLabel;
+  private
+  protected
+  public
+    procedure InitializeScene; override;
+    procedure FinalizeScene; override;
+    procedure TranslateTexts(const Language: string); override;
+  end;
+
+implementation
+
+{$R *.fmx}
+
+uses
+  System.Messaging,
+  uScene;
+
+{ TSceneGame }
+
+procedure TSceneGame.FinalizeScene;
+begin
+  inherited;
+  // TODO : à compléter
+end;
+
+procedure TSceneGame.InitializeScene;
+begin
+  inherited;
+  // TODO : à compléter
+end;
+
+procedure TSceneGame.TranslateTexts(const Language: string);
+begin
+  inherited;
+  // TODO : à compléter
+end;
+
+initialization
+
+TMessageManager.DefaultManager.SubscribeToMessage(TSceneFactory,
+  procedure(const Sender: TObject; const Msg: TMessage)
+  var
+    NewScene: TSceneGame;
+  begin
+    if (Msg is TSceneFactory) and
+      ((Msg as TSceneFactory).SceneType = TSceneType.Game) then
+    begin
+      NewScene := TSceneGame.Create(application.mainform);
+      NewScene.Parent := application.mainform;
+      tscene.RegisterScene(TSceneType.Game, NewScene);
+    end;
+  end);
+
+end.
