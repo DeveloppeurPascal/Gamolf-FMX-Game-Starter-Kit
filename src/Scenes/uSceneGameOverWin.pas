262b768230b8a8afb3a349272e33393b0e2585b4
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-07 21:48:05 +0200

    added the winner screen (#42)

---
+++ src/Scenes/uSceneGameOverWin.pas
@@ -0,0 +1,74 @@
+unit uSceneGameOverWin;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Graphics,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Dialogs,
+  FMX.StdCtrls,
+  _SceneAncestor, FMX.Controls.Presentation;
+
+type
+  TSceneGameOverWin = class(T__SceneAncestor)
+    Label1: TLabel;
+  private
+  protected
+  public
+    procedure InitializeScene; override;
+    procedure FinalizeScene; override;
+    procedure TranslateTexts(const Language: string); override;
+  end;
+
+implementation
+
+{$R *.fmx}
+
+uses
+  System.Messaging,
+  uScene;
+
+{ TSceneGameOverWin }
+
+procedure TSceneGameOverWin.FinalizeScene;
+begin
+  inherited;
+  // TODO : à compléter
+end;
+
+procedure TSceneGameOverWin.InitializeScene;
+begin
+  inherited;
+  // TODO : à compléter
+end;
+
+procedure TSceneGameOverWin.TranslateTexts(const Language: string);
+begin
+  inherited;
+  // TODO : à compléter
+end;
+
+initialization
+
+TMessageManager.DefaultManager.SubscribeToMessage(TSceneFactory,
+  procedure(const Sender: TObject; const Msg: TMessage)
+  var
+    NewScene: TSceneGameOverWin;
+  begin
+    if (Msg is TSceneFactory) and
+      ((Msg as TSceneFactory).SceneType = TSceneType.GameOverWin) then
+    begin
+      NewScene := TSceneGameOverWin.Create(application.mainform);
+      NewScene.Parent := application.mainform;
+      tscene.RegisterScene(TSceneType.GameOverWin, NewScene);
+    end;
+  end);
+
+end.
