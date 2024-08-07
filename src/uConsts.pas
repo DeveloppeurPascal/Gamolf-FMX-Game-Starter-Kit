7970d838e3af56f45681ce39b1654c560feb86fc
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-07 19:25:44 +0200

    added a condition for the FullScreen mode (DEBUG or RELEASE)

--- src/uConsts.pas
+++ src/uConsts.pas
@@ -8,9 +8,9 @@ interface
 const
   /// <summary>
   /// Version number of your game, don't forget to update the
-  /// Project/Options/Version sinfos before compiling a public RELEASE
+  /// Project/Options/Versions infos before compiling a public RELEASE
   /// </summary>
-  CAboutVersionNumber = '1.0';
+  CAboutVersionNumber = '0.0';
 
   /// <summary>
   /// Version date of your game, change it when you publish a new public release
@@ -41,6 +41,21 @@ const
   /// use 2 letters ISO code
   /// </remarks>
   CDefaultLanguage = 'en';
+
+  /// <summary>
+  /// If true the main form is opened in full screen. A menu with a "about" option is added for macOS.
+  /// If false the main form is what you did with it, no menu is added for macOS.
+  /// </summary>
+  /// <remarks>
+  /// Only available for desktop platforms. For iOS and Android it's "fullscreen" by design
+  /// </remarks>
+{$IFDEF DEBUG}
+  // In DEBUG mode
+  COpenGameInFullScreenMode = true;
+{$ELSE}
+  // For other compilations (including RELEASE)
+  COpenGameInFullScreenMode = true;
+{$ENDIF}
 
 implementation
 
