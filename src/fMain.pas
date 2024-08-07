2fb96d252cb1d94dad6d7916a0c52b74481fbe60
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-07 18:05:21 +0200

    added the F1 key management to open the default dialog box (#65)

--- src/fMain.pas
+++ src/fMain.pas
@@ -3,11 +3,21 @@ unit fMain;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Graphics,
+  FMX.Dialogs;
 
 type
   TfrmMain = class(TForm)
+    procedure FormKeyDown(Sender: TObject; var Key: Word; var KeyChar: WideChar;
+      Shift: TShiftState);
   private
     { Déclarations privées }
   public
@@ -20,5 +30,22 @@ var
 implementation
 
 {$R *.fmx}
+
+uses
+  uDMAboutBox;
+
+procedure TfrmMain.FormKeyDown(Sender: TObject; var Key: Word;
+  var KeyChar: WideChar; Shift: TShiftState);
+begin
+  if (Key = vkF1) and (KeyChar = #0) and (Shift = []) then
+    TAboutBox.ShowModal;
+end;
+
+initialization
+
+{$IFDEF DEBUG}
+  ReportMemoryLeaksOnShutdown := true;
+{$ENDIF}
+randomize;
 
 end.
