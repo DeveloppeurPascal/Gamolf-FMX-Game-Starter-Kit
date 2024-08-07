267b2e1817c03704e67e0e658bd19cc59c762499
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-07 18:01:32 +0200

    added a unit to manage program default settings and behaviour

---
+++ src/uConsts.pas
@@ -0,0 +1,47 @@
+unit uConsts;
+
+interface
+
+// TODO : personalize the game global settings
+{$MESSAGE WARN 'Personalize these constants'}
+
+const
+  /// <summary>
+  /// Version number of your game, don't forget to update the
+  /// Project/Options/Version sinfos before compiling a public RELEASE
+  /// </summary>
+  CAboutVersionNumber = '1.0';
+
+  /// <summary>
+  /// Version date of your game, change it when you publish a new public release
+  /// </summary>
+  CAboutVersionDate = '20240807';
+
+  /// <summary>
+  /// Title of your game used in the About box and as the main form caption
+  /// </summary>
+  CAboutGameTitle = 'My Game Template';
+
+  /// <summary>
+  /// The copyright to show in the About box
+  /// </summary>
+  CAboutCopyright = ''; // 2024 your name or anything else
+
+  /// <summary>
+  /// The website URL of your game (used in the About box)
+  /// </summary>
+  CAboutGameURL = '';
+
+  /// <summary>
+  /// Default language used if the system language is not supported
+  /// (of course you have to translate all textes of the program in this
+  /// language, so use yours or English by default)
+  /// </summary>
+  /// <remarks>
+  /// use 2 letters ISO code
+  /// </remarks>
+  CDefaultLanguage = 'en';
+
+implementation
+
+end.
