0a6ea2977e2ae303e337fb22f740dea290eafad0
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-07 21:47:50 +0200

    added the game over (lost) screen (#43)

---
+++ src/Scenes/uSceneGameOverLost.pas
@@ -0,0 +1,74 @@
+unit uSceneGameOverLost;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Graphics,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Dialogs,
+  FMX.StdCtrls,
+  _SceneAncestor, FMX.Controls.Presentation;
+
+type
+  TSceneGameOverLost = class(T__SceneAncestor)
+    Label1: TLabel;
+  private
+  protected
+  public
+    procedure InitializeScene; override;
+    procedure FinalizeScene; override;
+    procedure TranslateTexts(const Language: string); override;
+  end;
+
+implementation
+
+{$R *.fmx}
+
+uses
+  System.Messaging,
+  uScene;
+
+{ TSceneGameOverLost }
+
+procedure TSceneGameOverLost.FinalizeScene;
+begin
+  inherited;
+  // TODO : à compléter
+end;
+
+procedure TSceneGameOverLost.InitializeScene;
+begin
+  inherited;
+  // TODO : à compléter
+end;
+
+procedure TSceneGameOverLost.TranslateTexts(const Language: string);
+begin
+  inherited;
+  // TODO : à compléter
+end;
+
+initialization
+
+TMessageManager.DefaultManager.SubscribeToMessage(TSceneFactory,
+  procedure(const Sender: TObject; const Msg: TMessage)
+  var
+    NewScene: TSceneGameOverLost;
+  begin
+    if (Msg is TSceneFactory) and
+      ((Msg as TSceneFactory).SceneType = TSceneType.GameOverLost) then
+    begin
+      NewScene := TSceneGameOverLost.Create(application.mainform);
+      NewScene.Parent := application.mainform;
+      tscene.RegisterScene(TSceneType.GameOverLost, NewScene);
+    end;
+  end);
+
+end.
