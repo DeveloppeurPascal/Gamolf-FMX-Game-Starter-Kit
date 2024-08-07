ca04796a555e645c0bcb0f2644bf9ddc6caa5efc
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-07 19:26:35 +0200

    managed the "about" option menu for macOs (#54) and implemented the translation feature in main form

--- src/fMain.pas
+++ src/fMain.pas
@@ -4,6 +4,7 @@ interface
 
 uses
   System.SysUtils,
+  System.Messaging,
   System.Types,
   System.UITypes,
   System.Classes,
@@ -12,13 +13,22 @@ uses
   FMX.Controls,
   FMX.Forms,
   FMX.Graphics,
-  FMX.Dialogs;
+  FMX.Dialogs,
+  FMX.Menus;
 
 type
   TfrmMain = class(TForm)
+    MainMenu: TMainMenu;
+    mnuMacOSAppname: TMenuItem;
+    mnuAbout: TMenuItem;
     procedure FormKeyDown(Sender: TObject; var Key: Word; var KeyChar: WideChar;
       Shift: TShiftState);
+    procedure mnuAboutClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
   private
+  protected
+    procedure TranslateTexts(const Sender: TObject; const Msg: TMessage);
   public
   end;
 
@@ -30,13 +40,76 @@ implementation
 {$R *.fmx}
 
 uses
-  uDMAboutBox;
+  uDMAboutBox,
+  uConsts,
+  uTranslate;
+
+procedure TfrmMain.FormCreate(Sender: TObject);
+begin
+  fullscreen := COpenGameInFullScreenMode;
+{$IF Defined(MACOS) and not Defined(IOS)}
+{$ELSE}
+  freeandnil(MainMenu);
+{$ENDIF}
+  TMessageManager.DefaultManager.SubscribeToMessage(TTranslateTextsMessage,
+    TranslateTexts);
+end;
+
+procedure TfrmMain.FormDestroy(Sender: TObject);
+begin
+  TMessageManager.DefaultManager.Unsubscribe(TTranslateTextsMessage,
+    TranslateTexts, true);
+end;
 
 procedure TfrmMain.FormKeyDown(Sender: TObject; var Key: Word;
   var KeyChar: WideChar; Shift: TShiftState);
 begin
   if (Key = vkF1) and (KeyChar = #0) and (Shift = []) then
     TAboutBox.ShowModal;
+end;
+
+procedure TfrmMain.mnuAboutClick(Sender: TObject);
+begin
+  TAboutBox.ShowModal;
+end;
+
+procedure TfrmMain.TranslateTexts(const Sender: TObject; const Msg: TMessage);
+// var
+// ttm: TTranslateTextsMessage;
+begin
+  if not assigned(self) then
+    exit;
+
+  if assigned(Msg) and (Msg is TTranslateTextsMessage) then
+  begin
+    if assigned(MainMenu) then
+      mnuAbout.Text := TAboutBox.GetCaption;
+
+    // Uncomment and fill this code to translate texts on this form
+
+    // ttm := Msg as TTranslateTextsMessage;
+    // if ttm.Language = 'en' then
+    // begin
+    // end
+    // else if ttm.Language = 'fr' then
+    // begin
+    // end
+    // else if ttm.Language = 'it' then
+    // begin
+    // end
+    // else if ttm.Language = 'pt' then
+    // begin
+    // end
+    // else if ttm.Language = 'sp' then
+    // begin
+    // end
+    // else if ttm.Language = 'de' then
+    // begin
+    // end
+    // else
+    // raise Exception.Create('Sorry, I can''t translate in "' +
+    // ttm.Language + '".');
+  end;
 end;
 
 initialization
