be8bb025c5b79bdff16acf5e71fc61bc76025225
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-07 16:05:41 +0200

    created an empty FireMonkey project

---
+++ src/MyGameTemplate.dpr
@@ -0,0 +1,14 @@
+program MyGameTemplate;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  fMain in 'fMain.pas' {frmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
