3fdc7e53eaffa5f8c4e3d9802f9f85b7afbd2488
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-10 11:18:01 +0200

    updated the button ancestor class and added XML doc comments on it

--- starter-kit-src/_AncestorsForScenesAndUIElements/_ButtonsAncestor.pas
+++ starter-kit-src/_AncestorsForScenesAndUIElements/_ButtonsAncestor.pas
@@ -1,7 +1,7 @@
 ﻿/// <summary>
 /// ***************************************************************************
 ///
-/// Make games in Delphi (2024 edition) - Dev Days of Summer 2024
+/// Gamolf FMX Game Starter Kit
 ///
 /// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
 ///
@@ -15,11 +15,13 @@
 ///
 /// ***************************************************************************
 ///
-/// Samples projects for the "Make games in Delphi (2024 Edition)" talk at
-/// <Dev Days of Summer> 2024 online conference.
+/// The "Gamolf FMX Game Starter Kit" is both a "technical" example of a video
+/// game developed in Delphi with everything you need inside and a reusable
+/// project template you can customize for your own games.
 ///
-/// The projects are based on the "Gamolf FMX Game Template" you can find at
-/// https://gametemplate.developpeur-pascal.fr/
+/// The files provided are fully functional. Numerous comments are included in
+/// the sources to explain how they work and what you need to copy, override
+/// or customize to make video games without starting from scratch.
 ///
 /// ***************************************************************************
 ///
@@ -27,20 +29,38 @@
 /// Patrick PREMARTIN
 ///
 /// Site :
-/// https://serialstreameur.fr/make-games-in-delphi-2024-edition.html
+/// https://fmxgamestarterkit.developpeur-pascal.fr/
 ///
 /// Project site :
-/// https://github.com/DeveloppeurPascal/DevDaysOfSummer2024-MakeGamesInDelphi
+/// https://github.com/DeveloppeurPascal/Gamolf-FMX-Game-Starter-Kit
 ///
 /// ***************************************************************************
-/// File last update : 2024-08-09T23:59:48.000+02:00
-/// Signature : c62906edb264afe47fb85e02786bdff71d4ff236
+/// File last update : 2024-08-10T10:43:38.000+02:00
+/// Signature : 670323c3742b357a9dad8c0599f1a2d370372112
 /// ***************************************************************************
 /// </summary>
 
 unit _ButtonsAncestor;
 
 interface
+
+// If you want to be able to update the template files in your game project,
+// we recommend that you don't modify this file. Its operation should support
+// all standard use cases. Save the file in your project and work on the copy.
+// In this case, we suggest you open a ticket on the code repository to explain
+// your needs and the changes to be made to the template.
+//
+// The starter kit manage "real" buttons (FMX TControl descendants which can be
+// focused) or other controls implementing IUIControl interface.
+//
+// To simplify your life when you want a customized UI button :
+// - inherits from this frame,
+// - draw your button states (up by default, down when clicked, with and without
+// the focus) in the form designer,
+// - override the Repaint() method to manage visibility of your elements
+// depending on IsDown and IsFocused properties,
+// - use your new button as it or inherits from it to have a frame for each
+// text/icon button to show them with the good shape when designing your scenes
 
 uses
   System.SysUtils,
@@ -53,6 +73,13 @@ uses
   uUIElements;
 
 type
+  /// <summary>
+  /// Should be used as your UI buttons ancestor.
+  /// </summary>
+  /// <remarks>
+  /// Override Repaint() method in your descendants or you'll get EAbstractError
+  /// exceptions.
+  /// </remarks>
   T__ButtonAncestor = class(TFrame, IUIControl)
     procedure FrameMouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Single);
@@ -61,23 +88,83 @@ type
     FIsFocused: boolean;
     FIsDown: boolean;
     FText: string;
+    FTimeBetweenDownAndUpStatesWhenClicked: Integer;
     procedure SetIsDown(const Value: boolean);
     procedure SetIsFocused(const Value: boolean);
     procedure SetText(const Value: string);
+    function GetIsUp: boolean;
+    procedure SetIsUp(const Value: boolean);
+    procedure SetTimeBetweenDownAndUpStatesWhenClicked(const Value: Integer);
+    function GetTimeBetweenDownAndUpStatesWhenClicked: Integer;
   protected
     procedure DoTranslateTexts(const Sender: TObject; const Msg: TMessage);
   public
+    /// <summary>
+    /// Manage the focus state of this button
+    /// </summary>
     property IsFocused: boolean read FIsFocused write SetIsFocused;
+    /// <summary>
+    /// Manage the status of this button (False by default)
+    /// </summary>
     property IsDown: boolean read FIsDown write SetIsDown;
+    /// <summary>
+    /// Manage the status of this button (True by default)
+    /// </summary>
+    property IsUp: boolean read GetIsUp write SetIsUp;
+    /// <summary>
+    /// Sleeping time between Down and up states when the Click method is called.
+    /// Value is in milliseconds, used by the sleep() instruction.
+    /// </summary>
+    /// <remarks>
+    /// Default value is -1 to use CTimeInMSBetweenButtonDownAndUp value from
+    /// the game settings.
+    /// </remarks>
+    property TimeBetweenDownAndUpStatesWhenClicked: Integer
+      read GetTimeBetweenDownAndUpStatesWhenClicked
+      write SetTimeBetweenDownAndUpStatesWhenClicked;
+    /// <summary>
+    /// The text shown on this button
+    /// </summary>
     property Text: string read FText write SetText;
-    procedure AfterConstruction; override;
-    procedure BeforeDestruction; override;
-    constructor Create(AOwner: TComponent); override;
+    /// <summary>
+    /// Where you can translate the text of your button
+    /// (override and call it in your descendants)
+    /// </summary>
     procedure TranslateTexts(const Language: string); virtual;
+    /// <summary>
+    /// Called when the button is clicked.
+    /// - activate the down status
+    /// - wait for TimeBetweenDownAndUpStatesWhenClicked ms
+    /// - activate the up status
+    /// - call the onClick() event if it's not empty
+    /// (you can override it but it's better to fill the onClick event)
+    /// </summary>
     procedure Click; override;
+    /// <summary>
+    /// mostly internal use, change then IsFocused property is better
+    /// </summary>
     procedure SetFocus; virtual;
+    /// <summary>
+    /// mostly internal use, change then IsFocused property is better
+    /// </summary>
     procedure ResetFocus; virtual;
+    /// <summary>
+    /// Called to refresh the button on screen, override it in your descendants
+    /// or it will crash !
+    /// </summary>
     procedure Repaint; virtual; abstract;
+    /// <summary>
+    /// internal use, call it only if you override it in a descendant
+    /// </summary>
+    procedure AfterConstruction; override;
+    /// <summary>
+    /// internal use, call it only if you override it in a descendant
+    /// </summary>
+    procedure BeforeDestruction; override;
+    /// <summary>
+    /// Returns an instance of this class
+    /// </summary>
+    constructor Create(AOwner: TComponent); override;
   end;
 
 implementation
@@ -86,13 +173,13 @@ implementation
 
 uses
   uConfig,
-  uTranslate;
-
-{ TcadButton }
+  uTranslate,
+  uConsts;
 
 procedure T__ButtonAncestor.AfterConstruction;
 begin
   inherited;
+  name := '';
   tthread.ForceQueue(nil,
     procedure
     begin
@@ -112,23 +199,28 @@ end;
 
 procedure T__ButtonAncestor.Click;
 begin
-  IsDown := true;
-  IsFocused := true;
-  tthread.CreateAnonymousThread(
-    procedure
-    begin
-      sleep(50);
-      tthread.queue(nil,
-        procedure
-        begin
-          if not assigned(self) then
-            exit;
+  if GetTimeBetweenDownAndUpStatesWhenClicked < 1 then
+    inherited
+  else
+  begin
+    IsDown := true;
+    IsFocused := true;
+    tthread.CreateAnonymousThread(
+      procedure
+      begin
+        sleep(GetTimeBetweenDownAndUpStatesWhenClicked);
+        tthread.queue(nil,
+          procedure
+          begin
+            if not assigned(self) then
+              exit;
 
-          IsDown := false;
+            IsDown := false;
 
-          inherited;
-        end);
-    end).Start;
+            inherited;
+          end);
+      end).Start;
+  end;
 end;
 
 constructor T__ButtonAncestor.Create(AOwner: TComponent);
@@ -136,6 +228,8 @@ begin
   inherited;
   FIsFocused := false;
   FIsDown := false;
+  FText := '';
+  FTimeBetweenDownAndUpStatesWhenClicked := -1;
 end;
 
 procedure T__ButtonAncestor.DoTranslateTexts(const Sender: TObject;
@@ -157,6 +251,19 @@ end;
 procedure T__ButtonAncestor.FrameMouseLeave(Sender: TObject);
 begin
   IsDown := false;
+end;
+
+function T__ButtonAncestor.GetIsUp: boolean;
+begin
+  result := not IsDown;
+end;
+
+function T__ButtonAncestor.GetTimeBetweenDownAndUpStatesWhenClicked: Integer;
+begin
+  if FTimeBetweenDownAndUpStatesWhenClicked < 0 then
+    result := CTimeInMSBetweenButtonDownAndUp
+  else
+    result := FTimeBetweenDownAndUpStatesWhenClicked;
 end;
 
 procedure T__ButtonAncestor.ResetFocus;
@@ -181,10 +288,21 @@ begin
   Repaint;
 end;
 
+procedure T__ButtonAncestor.SetIsUp(const Value: boolean);
+begin
+  IsDown := not Value;
+end;
+
 procedure T__ButtonAncestor.SetText(const Value: string);
 begin
   FText := Value;
   Repaint;
+end;
+
+procedure T__ButtonAncestor.SetTimeBetweenDownAndUpStatesWhenClicked
+  (const Value: Integer);
+begin
+  FTimeBetweenDownAndUpStatesWhenClicked := Value;
 end;
 
 procedure T__ButtonAncestor.TranslateTexts(const Language: string);
