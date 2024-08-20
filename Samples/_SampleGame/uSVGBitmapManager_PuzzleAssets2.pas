fcba90d0d9215fd8a696db9a8ba2bb233ee9ad47
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-20 10:45:18 +0200

    added puzzle assets in sample game project (#83)

---
+++ Samples/_SampleGame/uSVGBitmapManager_PuzzleAssets2.pas
@@ -0,0 +1,81 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Gamolf FMX Game Starter Kit
+///
+/// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// The "Gamolf FMX Game Starter Kit" is both a "technical" example of a video
+/// game developed in Delphi with everything you need inside and a reusable
+/// project template you can customize for your own games.
+///
+/// The files provided are fully functional. Numerous comments are included in
+/// the sources to explain how they work and what you need to copy, override
+/// or customize to make video games without starting from scratch.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://fmxgamestarterkit.developpeur-pascal.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Gamolf-FMX-Game-Starter-Kit
+///
+/// ***************************************************************************
+/// File last update : 2024-08-10T11:32:22.000+02:00
+/// Signature : 5dfef073154dc8f3d4940af68d046214168df675
+/// ***************************************************************************
+/// </summary>
+
+unit uSVGBitmapManager_PuzzleAssets2;
+
+interface
+
+{$MESSAGE WARN 'Remove this file from your projects, it's only used as a sample or in some buttons. See uSVGBitmapManager_InputPrompts.pas if you need to use the SVGToBitmap features.'}
+// TODO : Remove this file from your projects, it's only used as a sample or in some buttons. See uSVGBitmapManager_InputPrompts.pas if you need to use the SVGToBitmap features.
+
+uses
+  FMX.Graphics,
+  USVGPuzzleAssets2;
+
+/// <summary>
+/// Returns a bitmap from a SVG image
+/// </summary>
+function getBitmapFromSVG(const Index: TSVGPuzzleAssets2Index;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+
+implementation
+
+uses
+  Olf.Skia.SVGToBitmap;
+
+function getBitmapFromSVG(const Index: TSVGPuzzleAssets2Index;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+begin
+  result := TOlfSVGBitmapList.Bitmap(ord(Index) + TSVGPuzzleAssets2.Tag,
+    round(width), round(height), BitmapScale);
+end;
+
+procedure RegisterSVGImages;
+begin
+  TSVGPuzzleAssets2.Tag := TOlfSVGBitmapList.AddItem(SVGPuzzleAssets2);
+end;
+
+initialization
+
+RegisterSVGImages;
+
+end.
