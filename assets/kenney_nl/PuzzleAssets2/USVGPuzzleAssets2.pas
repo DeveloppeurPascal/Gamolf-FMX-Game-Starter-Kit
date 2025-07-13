540be01ad40891d4f16ae88fcd1557a2921f8aaf
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-07-13 16:19:04 +0200

    regenerated the SVG storrage and bitmap creation units

--- assets/kenney_nl/PuzzleAssets2/USVGPuzzleAssets2.pas
+++ assets/kenney_nl/PuzzleAssets2/USVGPuzzleAssets2.pas
@@ -35,8 +35,8 @@
   https://github.com/DeveloppeurPascal/Gamolf-FMX-Game-Starter-Kit
 
   ***************************************************************************
-  File last update : 2025-02-09T11:12:48.265+01:00
-  Signature : 8d025755cab70f0d482f57a2a90b5f896319a70f
+  File last update : 2025-07-13T16:04:27.494+02:00
+  Signature : 1569c76ce5087040c81d5e424914a962c94b390c
   ***************************************************************************
 *)
 
@@ -44,23 +44,35 @@ unit USVGPuzzleAssets2;
 
 // ****************************************
 // * SVG from folder :
-// * C:\Users\patrickpremartin\Documents\Embarcadero\Studio\Projets\DDOS2024-GFGT\assets\kenney_nl\PuzzleAssets2\USVGPuzzleAssets2.pas
+// * .\
 // ****************************************
 //
-// This file contains a list of contants and 
-// an enumeration to access to SVG source codes 
+// This file contains a list of constants and
+// an enumeration to access to SVG source codes
 // from the generated array of strings.
 //
 // ****************************************
-// File generator : SVG Folder to Delphi Unit v1.0
-// Website : https://svgfolder2delphiunit.olfsoftware.fr/
-// Generation date : 2024-08-10T11:29:08.351Z
+// File generator : SVG Folder To Delphi Unit v2.0
+// Website : https://svgfolder2delphiunit.olfsoftware.fr
+// Generation date : 2025-07-13T16:04:27.481Z
 //
 // Don't do any change on this file.
 // They will be erased by next generation !
 // ****************************************
 
 interface
+
+{$IF Defined(FRAMEWORK_VCL)}
+
+uses
+  VCL.Graphics;
+{$ELSE IF Defined(FRAMEWORK_FMX)}
+
+uses
+  FMX.Graphics;
+{$ELSE}
+{$MESSAGE FATAL 'Is it a VCL or FMX program ?'}
+{$ENDIF}
 
 const
   CSVGBtnFocus = 0;
@@ -82,6 +94,7 @@ type
     FTagFloat: Single;
     FTagObject: TObject;
     FTagString: string;
+    FBitmapListIndex: integer;
     class procedure SetTag(const Value: integer); static;
     class procedure SetTagBool(const Value: Boolean); static;
     class procedure SetTagFloat(const Value: Single); static;
@@ -100,14 +113,23 @@ type
     class function SVG(const Index: TSVGPuzzleAssets2Index) : string; overload;
     class function Count : Integer;
     class constructor Create;
+    class function Bitmap(const Index: TSVGPuzzleAssets2Index;
+      const width, height: single; const BitmapScale: single): TBitmap;
   end;
 
 var
   SVGPuzzleAssets2 : array of String;
 
+/// <summary>
+/// Returns a bitmap from a TSVGPuzzleAssets2 SVG file
+/// </summary>
+function getBitmapFromSVG(const Index: TSVGPuzzleAssets2Index;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+
 implementation
 
 uses
+  Olf.Skia.SVGToBitmap,
   System.SysUtils;
 
 { TSVGPuzzleAssets2 }
@@ -115,6 +137,7 @@ uses
 class constructor TSVGPuzzleAssets2.Create;
 begin
   inherited;
+  FBitmapListIndex := 0;
   FTag := 0;
   FTagBool := false;
   FTagFloat := 0;
@@ -165,19 +188,51 @@ begin
   result := length(SVGPuzzleAssets2);
 end;
 
+class function TSVGPuzzleAssets2.Bitmap(const Index: TSVGPuzzleAssets2Index;
+  const width, height: single; const BitmapScale: single): TBitmap;
+begin
+  result := TOlfSVGBitmapList.Bitmap(ord(Index) + TSVGPuzzleAssets2.FBitmapListIndex,
+    round(width), round(height), BitmapScale);
+end;
+
+procedure RegisterSVGBitmap;
+begin
+  TSVGPuzzleAssets2.FBitmapListIndex := TOlfSVGBitmapList.AddItem(SVGPuzzleAssets2);
+end;
+
+function getBitmapFromSVG(const Index: TSVGPuzzleAssets2Index;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+begin
+  Result := TSVGPuzzleAssets2.Bitmap(Index, width, height, BitmapScale);
+end;
+
 initialization
 
 SetLength(SVGPuzzleAssets2, 3);
 
-{$TEXTBLOCK NATIVE XML}
-SVGPuzzleAssets2[CSVGBtnFocus] := '''
-<?xml version="1.0" encoding="UTF-8"?><svg id="Calque_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 104 54"><g id="Calque_1-2"><path d="M104,5v44c0,1.37-.48,2.55-1.45,3.55-1,.97-2.18,1.45-3.55,1.45H5c-1.37,0-2.53-.48-3.5-1.45s-1.5-2.18-1.5-3.55V5c0-1.37.5-2.53,1.5-3.5C2.47.5,3.63,0,5,0h94c1.37,0,2.55.5,3.55,1.5.97.97,1.45,2.13,1.45,3.5M99,5H5v44h94V5" fill="#fff" stroke-width="0"/><path d="M13,13h78v28H13V13" fill="#f9c" stroke-width="0"/><path d="M99,5l-8,8H13L5,5h94" fill="#ffd2e8" stroke-width="0"/><path d="M91,13l8-8v44l-8-8V13M5,5l8,8v28l-8,8V5" fill="#ffaad4" stroke-width="0"/><path d="M91,41l8,8H5l8-8h78" fill="#f080b8" stroke-width="0"/></g></svg>
-''';
-SVGPuzzleAssets2[CSVGBtnOff] := '''
-<?xml version="1.0" encoding="UTF-8"?><svg id="Calque_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 104 54"><g id="Calque_1-2"><path d="M13,41V13h78v28H13" fill="#f9c" stroke-width="0"/><path d="M13,13L5,5h94l-8,8H13" fill="#ffd2e8" stroke-width="0"/><path d="M13,13v28l-8,8V5l8,8M99,5v44l-8-8V13l8-8" fill="#ffaad4" stroke-width="0"/><path d="M5,49l8-8h78l8,8H5" fill="#f080b8" stroke-width="0"/><path d="M5,49h94V5H5v44M5,0h94c1.37,0,2.55.5,3.55,1.5s1.45,2.13,1.45,3.5v44c0,1.37-.48,2.55-1.45,3.55-1,.97-2.18,1.45-3.55,1.45H5c-1.37,0-2.53-.48-3.5-1.45s-1.5-2.18-1.5-3.55V5c0-1.37.5-2.53,1.5-3.5.97-1,2.13-1.5,3.5-1.5" fill="#c45a8f" stroke-width="0"/></g></svg>
-''';
-SVGPuzzleAssets2[CSVGBtnOn] := '''
-<?xml version="1.0" encoding="UTF-8"?><svg id="Calque_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 94 44"><g id="Calque_1-2"><path d="M8,8h78v28H8V8" fill="#f9c" stroke-width="0"/><path d="M94,0l-8,8H8L0,0h94" fill="#ffd2e8" stroke-width="0"/><path d="M86,8L94,0v44l-8-8V8M0,0l8,8v28L0,44V0" fill="#ffaad4" stroke-width="0"/><path d="M86,36l8,8H0l8-8h78" fill="#f080b8" stroke-width="0"/></g></svg>
-''';
+SVGPuzzleAssets2[CSVGBtnFocus] := 
+  '<?xml version="1.0" encoding="UTF-8"?><svg id="Calque_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 104 54"><g id="Calque_1'
+  + '-2"><path d="M104,5v44c0,1.37-.48,2.55-1.45,3.55-1,.97-2.18,1.45-3.55,1.45H5c-1.37,0-2.53-.48-3.5-1.45s-1.5-2.18-1.5-3.55V5c0-1.'
+  + '37.5-2.53,1.5-3.5C2.47.5,3.63,0,5,0h94c1.37,0,2.55.5,3.55,1.5.97.97,1.45,2.13,1.45,3.5M99,5H5v44h94V5" fill="#fff" stroke-width='
+  + '"0"/><path d="M13,13h78v28H13V13" fill="#f9c" stroke-width="0"/><path d="M99,5l-8,8H13L5,5h94" fill="#ffd2e8" stroke-width="0"/>'
+  + '<path d="M91,13l8-8v44l-8-8V13M5,5l8,8v28l-8,8V5" fill="#ffaad4" stroke-width="0"/><path d="M91,41l8,8H5l8-8h78" fill="#f080b8" '
+  + 'stroke-width="0"/></g></svg>'
+;
+SVGPuzzleAssets2[CSVGBtnOff] := 
+  '<?xml version="1.0" encoding="UTF-8"?><svg id="Calque_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 104 54"><g id="Calque_1'
+  + '-2"><path d="M13,41V13h78v28H13" fill="#f9c" stroke-width="0"/><path d="M13,13L5,5h94l-8,8H13" fill="#ffd2e8" stroke-width="0"/>'
+  + '<path d="M13,13v28l-8,8V5l8,8M99,5v44l-8-8V13l8-8" fill="#ffaad4" stroke-width="0"/><path d="M5,49l8-8h78l8,8H5" fill="#f080b8" '
+  + 'stroke-width="0"/><path d="M5,49h94V5H5v44M5,0h94c1.37,0,2.55.5,3.55,1.5s1.45,2.13,1.45,3.5v44c0,1.37-.48,2.55-1.45,3.55-1,.97-2'
+  + '.18,1.45-3.55,1.45H5c-1.37,0-2.53-.48-3.5-1.45s-1.5-2.18-1.5-3.55V5c0-1.37.5-2.53,1.5-3.5.97-1,2.13-1.5,3.5-1.5" fill="#c45a8f" '
+  + 'stroke-width="0"/></g></svg>'
+;
+SVGPuzzleAssets2[CSVGBtnOn] := 
+  '<?xml version="1.0" encoding="UTF-8"?><svg id="Calque_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 94 44"><g id="Calque_1-'
+  + '2"><path d="M8,8h78v28H8V8" fill="#f9c" stroke-width="0"/><path d="M94,0l-8,8H8L0,0h94" fill="#ffd2e8" stroke-width="0"/><path d'
+  + '="M86,8L94,0v44l-8-8V8M0,0l8,8v28L0,44V0" fill="#ffaad4" stroke-width="0"/><path d="M86,36l8,8H0l8-8h78" fill="#f080b8" stroke-w'
+  + 'idth="0"/></g></svg>'
+;
+
+RegisterSVGBitmap;
 
 end.
--- assets/kenney_nl/UIPack/USVGUIPack.pas
+++ assets/kenney_nl/UIPack/USVGUIPack.pas
@@ -35,8 +35,8 @@
   https://github.com/DeveloppeurPascal/Gamolf-FMX-Game-Starter-Kit
 
   ***************************************************************************
-  File last update : 2025-02-09T11:12:48.265+01:00
-  Signature : 8b4bd74f03dcb97b8dc7610da810b8a46aea6499
+  File last update : 2025-07-13T16:04:57.198+02:00
+  Signature : 50b493e10cee2c2145cdae7fae410ec72e091ed9
   ***************************************************************************
 *)
 
@@ -44,23 +44,35 @@ unit USVGUIPack;
 
 // ****************************************
 // * SVG from folder :
-// * C:\Users\patrickpremartin\Documents\Embarcadero\Studio\Projets\gfgsk\assets\kenney_nl\UIPack\USVGUIPack.pas
+// * .\
 // ****************************************
 //
-// This file contains a list of contants and 
-// an enumeration to access to SVG source codes 
+// This file contains a list of constants and
+// an enumeration to access to SVG source codes
 // from the generated array of strings.
 //
 // ****************************************
-// File generator : SVG Folder to Delphi Unit v1.0
-// Website : https://svgfolder2delphiunit.olfsoftware.fr/
-// Generation date : 2024-08-21T10:30:39.111Z
+// File generator : SVG Folder To Delphi Unit v2.0
+// Website : https://svgfolder2delphiunit.olfsoftware.fr
+// Generation date : 2025-07-13T16:04:57.179Z
 //
 // Don't do any change on this file.
 // They will be erased by next generation !
 // ****************************************
 
 interface
+
+{$IF Defined(FRAMEWORK_VCL)}
+
+uses
+  VCL.Graphics;
+{$ELSE IF Defined(FRAMEWORK_FMX)}
+
+uses
+  FMX.Graphics;
+{$ELSE}
+{$MESSAGE FATAL 'Is it a VCL or FMX program ?'}
+{$ENDIF}
 
 const
   CSVGButtonRectangleDepthBorder = 0;
@@ -84,6 +96,7 @@ type
     FTagFloat: Single;
     FTagObject: TObject;
     FTagString: string;
+    FBitmapListIndex: integer;
     class procedure SetTag(const Value: integer); static;
     class procedure SetTagBool(const Value: Boolean); static;
     class procedure SetTagFloat(const Value: Single); static;
@@ -103,14 +116,23 @@ type
     class function SVG(const Index: TSVGUIPackIndex) : string; overload;
     class function Count : Integer;
     class constructor Create;
+    class function Bitmap(const Index: TSVGUIPackIndex;
+      const width, height: single; const BitmapScale: single): TBitmap;
   end;
 
 var
   SVGUIPack : array of String;
 
+/// <summary>
+/// Returns a bitmap from a TSVGUIPack SVG file
+/// </summary>
+function getBitmapFromSVG(const Index: TSVGUIPackIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+
 implementation
 
 uses
+  Olf.Skia.SVGToBitmap,
   System.SysUtils;
 
 { TSVGUIPack }
@@ -118,6 +140,7 @@ uses
 class constructor TSVGUIPack.Create;
 begin
   inherited;
+  FBitmapListIndex := 0;
   FTag := 0;
   FTagBool := false;
   FTagFloat := 0;
@@ -168,58 +191,113 @@ begin
   result := length(SVGUIPack);
 end;
 
+class function TSVGUIPack.Bitmap(const Index: TSVGUIPackIndex;
+  const width, height: single; const BitmapScale: single): TBitmap;
+begin
+  result := TOlfSVGBitmapList.Bitmap(ord(Index) + TSVGUIPack.FBitmapListIndex,
+    round(width), round(height), BitmapScale);
+end;
+
+procedure RegisterSVGBitmap;
+begin
+  TSVGUIPack.FBitmapListIndex := TOlfSVGBitmapList.AddItem(SVGUIPack);
+end;
+
+function getBitmapFromSVG(const Index: TSVGUIPackIndex;
+  const width, height: single; const BitmapScale: single): tbitmap; overload;
+begin
+  Result := TSVGUIPack.Bitmap(Index, width, height, BitmapScale);
+end;
+
 initialization
 
 SetLength(SVGUIPack, 4);
 
-{$TEXTBLOCK NATIVE XML}
-SVGUIPack[CSVGButtonRectangleDepthBorder] := '''
-<svg width="192" height="64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs/>
-  <g>
-    <path stroke="none" fill="#167DA8" d="M189.95 25.2 L189.95 24.8 190 24 190 6 189.95 5.05 Q189.5 2 186 2 L6 2 Q2.5 2 2.1 5.05 L2 6 2 24 2.05 24.8 2.05 25.2 2 26 2 54 Q2 58 6 58 L186 58 Q190 58 190 54 L190 26 189.95 25.2 M0 24.75 L0 6 Q0 0 6 0 L186 0 Q192 0 192 6 L192 24.75 191.95 25 192 25.4 192 54.25 Q191.85 60 186 60 L6 60 Q0.15 60 0 54.25 L0 25.4 0.05 25 0 24.75"/>
-    <path stroke="none" fill="#36BDF7" d="M189.95 25.2 L190 26 190 54 Q190 58 186 58 L6 58 Q2 58 2 54 L2 26 2.05 25.2 2.05 24.8 2 24 2 6 2.1 5.05 Q2.5 2 6 2 L186 2 Q189.5 2 189.95 5.05 L190 6 190 24 189.95 24.8 189.95 25.2 M6 4 Q4.35 4 4.1 5.3 L4.05 5.5 4 6 4 24 4.05 24.55 4.05 24.8 4.05 25.2 4.05 25.45 4 26 4 54 Q4 56 6 56 L186 56 Q188 56 188 54 L188 26 188 25.45 187.95 25.2 187.95 24.8 188 24.55 188 24 188 6 187.95 5.3 188 5.35 Q187.65 4 186 4 L6 4"/>
-    <path stroke="none" fill="#1C9FD7" d="M6 4 L186 4 Q187.65 4 188 5.35 L187.95 5.3 188 6 188 24 188 24.55 187.95 24.8 187.95 25.2 188 25.45 188 26 188 54 Q188 56 186 56 L6 56 Q4 56 4 54 L4 26 4.05 25.45 4.05 25.2 4.05 24.8 4.05 24.55 4 24 4 6 4.05 5.5 4.1 5.3 Q4.35 4 6 4 M84.7 6 L81.8 6 6 6 6 24 6 24.15 6.05 24.55 6.05 24.8 6.05 25.2 6.05 25.45 6.05 25.8 6 26 6 54 81.8 54 87.55 54 186 54 186 26 186 25.75 185.95 25.2 185.95 24.8 186 24.3 186 24 186 6 84.7 6"/>
-    <path stroke="none" fill="#FFFFFF" d="M84.7 6 L186 6 186 24 186 24.3 185.95 24.8 185.95 25.2 186 25.75 186 26 186 54 87.55 54 81.8 54 6 54 6 26 6.05 25.8 6.05 25.45 6.05 25.2 6.05 24.8 6.05 24.55 6 24.15 6 24 6 6 81.8 6 84.7 6 M183.95 25.05 L183.95 24.95 Q183.9 24.55 184 24.1 L184 24 184 8 8 8 8 24 8.05 24.5 8.05 24.55 8.05 24.8 8.05 25.2 8.05 25.45 8 26.1 8 52 184 52 184 26 184 25.9 183.95 25.05"/>
-    <path stroke="none" fill="#DADCE7" d="M183.95 25.05 L184 25.9 184 26 184 52 8 52 8 26.1 8.05 25.45 8.05 25.2 8.05 24.8 8.05 24.55 8.05 24.5 8 24 8 8 184 8 184 24 184 24.1 Q183.9 24.55 183.95 24.95 L183.95 25.05"/>
-    <path stroke="none" fill="#FF0000" d="M192 24.75 L192 25.4 191.95 25 192 24.75 M0 25.4 L0 24.75 0.05 25 0 25.4"/>
-    <path stroke="none" fill="#146587" d="M192 54.25 L192 58 Q192 64 186 64 L6 64 Q0 64 0 58 L0 54.25 Q0.15 60 6 60 L186 60 Q191.85 60 192 54.25"/>
-  </g>
-</svg>
-''';
-SVGUIPack[CSVGButtonRectangleDepthLine] := '''
-<svg width="192" height="64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs/>
-  <g>
-    <path stroke="none" fill="#9696A3" d="M0 24.75 L0 6 Q0 0 6 0 L186 0 Q192 0 192 6 L192 24.75 191.95 25 192 25.4 192 54.25 Q191.85 60 186 60 L6 60 Q0.15 60 0 54.25 L0 25.4 0.05 25 0 24.75 M189.95 25.2 L189.95 24.8 190 24 190 6 189.95 5.05 Q189.5 2 186 2 L6 2 Q2.5 2 2.1 5.05 L2 6 2 24 2.05 24.8 2.05 25.2 2 26 2 54 Q2 58 6 58 L186 58 Q190 58 190 54 L190 26 189.95 25.2"/>
-    <path stroke="none" fill="#FFFFFF" d="M189.95 25.2 L190 26 190 54 Q190 58 186 58 L6 58 Q2 58 2 54 L2 26 2.05 25.2 2.05 24.8 2 24 2 6 2.1 5.05 Q2.5 2 6 2 L186 2 Q189.5 2 189.95 5.05 L190 6 190 24 189.95 24.8 189.95 25.2 M6 4 Q4.35 4 4.1 5.3 L4.05 5.5 4 6 4 24 4.05 24.55 4.05 24.8 4.05 25.2 4.05 25.45 4 26 4 54 Q4 56 6 56 L186 56 Q188 56 188 54 L188 26 188 25.45 187.95 25.2 187.95 24.8 188 24.55 188 24 188 6 187.95 5.3 188 5.35 Q187.65 4 186 4 L6 4"/>
-    <path stroke="none" fill="#DADCE7" d="M6 4 L186 4 Q187.65 4 188 5.35 L187.95 5.3 188 6 188 24 188 24.55 187.95 24.8 187.95 25.2 188 25.45 188 26 188 54 Q188 56 186 56 L6 56 Q4 56 4 54 L4 26 4.05 25.45 4.05 25.2 4.05 24.8 4.05 24.55 4 24 4 6 4.05 5.5 4.1 5.3 Q4.35 4 6 4 M84.7 6 L81.8 6 6 6 6 24 6 24.15 6.05 24.55 6.05 24.8 6.05 25.2 6.05 25.45 6.05 25.8 6 26 6 54 81.8 54 87.55 54 186 54 186 26 186 25.75 185.95 25.2 185.95 24.8 186 24.3 186 24 186 6 84.7 6 M183.95 25.05 L184 25.9 184 26 184 52 8 52 8 26.1 8.05 25.45 8.05 25.2 8.05 24.8 8.05 24.55 8.05 24.5 8 24 8 8 184 8 184 24 184 24.1 Q183.9 24.55 183.95 24.95 L183.95 25.05"/>
-    <path stroke="none" fill="#36BDF7" d="M183.95 25.05 L183.95 24.95 Q183.9 24.55 184 24.1 L184 24 184 8 8 8 8 24 8.05 24.5 8.05 24.55 8.05 24.8 8.05 25.2 8.05 25.45 8 26.1 8 52 184 52 184 26 184 25.9 183.95 25.05 M84.7 6 L186 6 186 24 186 24.3 185.95 24.8 185.95 25.2 186 25.75 186 26 186 54 87.55 54 81.8 54 6 54 6 26 6.05 25.8 6.05 25.45 6.05 25.2 6.05 24.8 6.05 24.55 6 24.15 6 24 6 6 81.8 6 84.7 6"/>
-    <path stroke="none" fill="#FF0000" d="M192 24.75 L192 25.4 191.95 25 192 24.75 M0 25.4 L0 24.75 0.05 25 0 25.4"/>
-    <path stroke="none" fill="#747487" d="M192 54.25 L192 58 Q192 64 186 64 L6 64 Q0 64 0 58 L0 54.25 Q0.15 60 6 60 L186 60 Q191.85 60 192 54.25"/>
-  </g>
-</svg>
-''';
-SVGUIPack[CSVGButtonRectangleFlat] := '''
-<svg width="192" height="64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs/>
-  <g>
-    <path stroke="none" fill="#989AAF" d="M0 58.25 L0 5.95 Q0.05 0 6 0 L186 0 Q192 0 192 5.95 L192 58.25 Q191.85 64 186 64 L6 64 Q0.15 64 0 58.25 M186.5 2.05 L6 2 5.9 2 5.4 2.05 Q2 2.3 2 5.95 L2 58 Q2 62 6 62 L186 62 Q190 62 190 58 L190 5.95 Q190 2.25 186.5 2.05"/>
-    <path stroke="none" fill="#FFFFFF" d="M186.5 2.05 Q190 2.25 190 5.95 L190 58 Q190 62 186 62 L6 62 Q2 62 2 58 L2 5.95 Q2 2.3 5.4 2.05 L5.9 2 6 2 186.5 2.05 M186.4 4.05 L6.1 4 5.6 4.05 5.55 4.05 Q4 4.2 4 5.95 L4 58 Q4 60 6 60 L186 60 Q188 60 188 58 L188 5.95 Q188 4.2 186.4 4.05"/>
-    <path stroke="none" fill="#DADCE7" d="M186.4 4.05 Q188 4.2 188 5.95 L188 58 Q188 60 186 60 L6 60 Q4 60 4 58 L4 5.95 Q4 4.2 5.55 4.05 L5.6 4.05 6.1 4 186.4 4.05"/>
-  </g>
-</svg>
-''';
-SVGUIPack[CSVGButtonRectangleLine] := '''
-<svg width="192" height="64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs/>
-  <g>
-    <path stroke="none" fill="#9696A3" d="M0 58.25 L0 5.95 Q0.05 0 6 0 L186 0 Q192 0 192 5.95 L192 58.25 Q191.85 64 186 64 L6 64 Q0.15 64 0 58.25 M186.5 2.05 L6 2 5.9 2 5.4 2.05 Q2 2.3 2 5.95 L2 58 Q2 62 6 62 L186 62 Q190 62 190 58 L190 5.95 Q190 2.25 186.5 2.05"/>
-    <path stroke="none" fill="#FFFFFF" d="M186.5 2.05 Q190 2.25 190 5.95 L190 58 Q190 62 186 62 L6 62 Q2 62 2 58 L2 5.95 Q2 2.3 5.4 2.05 L5.9 2 6 2 186.5 2.05 M186.4 4.05 L6.1 4 5.6 4.05 5.55 4.05 Q4 4.2 4 5.95 L4 58 Q4 60 6 60 L186 60 Q188 60 188 58 L188 5.95 Q188 4.2 186.4 4.05"/>
-    <path stroke="none" fill="#DADCE7" d="M186.4 4.05 Q188 4.2 188 5.95 L188 58 Q188 60 186 60 L6 60 Q4 60 4 58 L4 5.95 Q4 4.2 5.55 4.05 L5.6 4.05 6.1 4 186.4 4.05 M6.3 6 L6 6.05 6 58 186 58 186 6.05 6.3 6 M184 8.05 L184 56 8 56 8 8 184 8.05"/>
-    <path stroke="none" fill="#36BDF7" d="M184 8.05 L8 8 8 56 184 56 184 8.05 M6.3 6 L186 6.05 186 58 6 58 6 6.05 6.3 6"/>
-  </g>
-</svg>
-''';
+SVGUIPack[CSVGButtonRectangleDepthBorder] := 
+  '<svg width="192" height="64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
+  + '  <defs/>'
+  + '  <g>'
+  + '    <path stroke="none" fill="#167DA8" d="M189.95 25.2 L189.95 24.8 190 24 190 6 189.95 5.05 Q189.5 2 186 2 L6 2 Q2.5 2 2.1 5.05'
+  + ' L2 6 2 24 2.05 24.8 2.05 25.2 2 26 2 54 Q2 58 6 58 L186 58 Q190 58 190 54 L190 26 189.95 25.2 M0 24.75 L0 6 Q0 0 6 0 L186 0 Q19'
+  + '2 0 192 6 L192 24.75 191.95 25 192 25.4 192 54.25 Q191.85 60 186 60 L6 60 Q0.15 60 0 54.25 L0 25.4 0.05 25 0 24.75"/>'
+  + '    <path stroke="none" fill="#36BDF7" d="M189.95 25.2 L190 26 190 54 Q190 58 186 58 L6 58 Q2 58 2 54 L2 26 2.05 25.2 2.05 24.8 '
+  + '2 24 2 6 2.1 5.05 Q2.5 2 6 2 L186 2 Q189.5 2 189.95 5.05 L190 6 190 24 189.95 24.8 189.95 25.2 M6 4 Q4.35 4 4.1 5.3 L4.05 5.5 4 '
+  + '6 4 24 4.05 24.55 4.05 24.8 4.05 25.2 4.05 25.45 4 26 4 54 Q4 56 6 56 L186 56 Q188 56 188 54 L188 26 188 25.45 187.95 25.2 187.9'
+  + '5 24.8 188 24.55 188 24 188 6 187.95 5.3 188 5.35 Q187.65 4 186 4 L6 4"/>'
+  + '    <path stroke="none" fill="#1C9FD7" d="M6 4 L186 4 Q187.65 4 188 5.35 L187.95 5.3 188 6 188 24 188 24.55 187.95 24.8 187.95 2'
+  + '5.2 188 25.45 188 26 188 54 Q188 56 186 56 L6 56 Q4 56 4 54 L4 26 4.05 25.45 4.05 25.2 4.05 24.8 4.05 24.55 4 24 4 6 4.05 5.5 4.'
+  + '1 5.3 Q4.35 4 6 4 M84.7 6 L81.8 6 6 6 6 24 6 24.15 6.05 24.55 6.05 24.8 6.05 25.2 6.05 25.45 6.05 25.8 6 26 6 54 81.8 54 87.55 5'
+  + '4 186 54 186 26 186 25.75 185.95 25.2 185.95 24.8 186 24.3 186 24 186 6 84.7 6"/>'
+  + '    <path stroke="none" fill="#FFFFFF" d="M84.7 6 L186 6 186 24 186 24.3 185.95 24.8 185.95 25.2 186 25.75 186 26 186 54 87.55 5'
+  + '4 81.8 54 6 54 6 26 6.05 25.8 6.05 25.45 6.05 25.2 6.05 24.8 6.05 24.55 6 24.15 6 24 6 6 81.8 6 84.7 6 M183.95 25.05 L183.95 24.'
+  + '95 Q183.9 24.55 184 24.1 L184 24 184 8 8 8 8 24 8.05 24.5 8.05 24.55 8.05 24.8 8.05 25.2 8.05 25.45 8 26.1 8 52 184 52 184 26 18'
+  + '4 25.9 183.95 25.05"/>'
+  + '    <path stroke="none" fill="#DADCE7" d="M183.95 25.05 L184 25.9 184 26 184 52 8 52 8 26.1 8.05 25.45 8.05 25.2 8.05 24.8 8.05 '
+  + '24.55 8.05 24.5 8 24 8 8 184 8 184 24 184 24.1 Q183.9 24.55 183.95 24.95 L183.95 25.05"/>'
+  + '    <path stroke="none" fill="#FF0000" d="M192 24.75 L192 25.4 191.95 25 192 24.75 M0 25.4 L0 24.75 0.05 25 0 25.4"/>'
+  + '    <path stroke="none" fill="#146587" d="M192 54.25 L192 58 Q192 64 186 64 L6 64 Q0 64 0 58 L0 54.25 Q0.15 60 6 60 L186 60 Q191'
+  + '.85 60 192 54.25"/>'
+  + '  </g>'
+  + '</svg>'
+;
+SVGUIPack[CSVGButtonRectangleDepthLine] := 
+  '<svg width="192" height="64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
+  + '  <defs/>'
+  + '  <g>'
+  + '    <path stroke="none" fill="#9696A3" d="M0 24.75 L0 6 Q0 0 6 0 L186 0 Q192 0 192 6 L192 24.75 191.95 25 192 25.4 192 54.25 Q19'
+  + '1.85 60 186 60 L6 60 Q0.15 60 0 54.25 L0 25.4 0.05 25 0 24.75 M189.95 25.2 L189.95 24.8 190 24 190 6 189.95 5.05 Q189.5 2 186 2 '
+  + 'L6 2 Q2.5 2 2.1 5.05 L2 6 2 24 2.05 24.8 2.05 25.2 2 26 2 54 Q2 58 6 58 L186 58 Q190 58 190 54 L190 26 189.95 25.2"/>'
+  + '    <path stroke="none" fill="#FFFFFF" d="M189.95 25.2 L190 26 190 54 Q190 58 186 58 L6 58 Q2 58 2 54 L2 26 2.05 25.2 2.05 24.8 '
+  + '2 24 2 6 2.1 5.05 Q2.5 2 6 2 L186 2 Q189.5 2 189.95 5.05 L190 6 190 24 189.95 24.8 189.95 25.2 M6 4 Q4.35 4 4.1 5.3 L4.05 5.5 4 '
+  + '6 4 24 4.05 24.55 4.05 24.8 4.05 25.2 4.05 25.45 4 26 4 54 Q4 56 6 56 L186 56 Q188 56 188 54 L188 26 188 25.45 187.95 25.2 187.9'
+  + '5 24.8 188 24.55 188 24 188 6 187.95 5.3 188 5.35 Q187.65 4 186 4 L6 4"/>'
+  + '    <path stroke="none" fill="#DADCE7" d="M6 4 L186 4 Q187.65 4 188 5.35 L187.95 5.3 188 6 188 24 188 24.55 187.95 24.8 187.95 2'
+  + '5.2 188 25.45 188 26 188 54 Q188 56 186 56 L6 56 Q4 56 4 54 L4 26 4.05 25.45 4.05 25.2 4.05 24.8 4.05 24.55 4 24 4 6 4.05 5.5 4.'
+  + '1 5.3 Q4.35 4 6 4 M84.7 6 L81.8 6 6 6 6 24 6 24.15 6.05 24.55 6.05 24.8 6.05 25.2 6.05 25.45 6.05 25.8 6 26 6 54 81.8 54 87.55 5'
+  + '4 186 54 186 26 186 25.75 185.95 25.2 185.95 24.8 186 24.3 186 24 186 6 84.7 6 M183.95 25.05 L184 25.9 184 26 184 52 8 52 8 26.1'
+  + ' 8.05 25.45 8.05 25.2 8.05 24.8 8.05 24.55 8.05 24.5 8 24 8 8 184 8 184 24 184 24.1 Q183.9 24.55 183.95 24.95 L183.95 25.05"/>'
+  + '    <path stroke="none" fill="#36BDF7" d="M183.95 25.05 L183.95 24.95 Q183.9 24.55 184 24.1 L184 24 184 8 8 8 8 24 8.05 24.5 8.0'
+  + '5 24.55 8.05 24.8 8.05 25.2 8.05 25.45 8 26.1 8 52 184 52 184 26 184 25.9 183.95 25.05 M84.7 6 L186 6 186 24 186 24.3 185.95 24.'
+  + '8 185.95 25.2 186 25.75 186 26 186 54 87.55 54 81.8 54 6 54 6 26 6.05 25.8 6.05 25.45 6.05 25.2 6.05 24.8 6.05 24.55 6 24.15 6 2'
+  + '4 6 6 81.8 6 84.7 6"/>'
+  + '    <path stroke="none" fill="#FF0000" d="M192 24.75 L192 25.4 191.95 25 192 24.75 M0 25.4 L0 24.75 0.05 25 0 25.4"/>'
+  + '    <path stroke="none" fill="#747487" d="M192 54.25 L192 58 Q192 64 186 64 L6 64 Q0 64 0 58 L0 54.25 Q0.15 60 6 60 L186 60 Q191'
+  + '.85 60 192 54.25"/>'
+  + '  </g>'
+  + '</svg>'
+;
+SVGUIPack[CSVGButtonRectangleFlat] := 
+  '<svg width="192" height="64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
+  + '  <defs/>'
+  + '  <g>'
+  + '    <path stroke="none" fill="#989AAF" d="M0 58.25 L0 5.95 Q0.05 0 6 0 L186 0 Q192 0 192 5.95 L192 58.25 Q191.85 64 186 64 L6 64'
+  + ' Q0.15 64 0 58.25 M186.5 2.05 L6 2 5.9 2 5.4 2.05 Q2 2.3 2 5.95 L2 58 Q2 62 6 62 L186 62 Q190 62 190 58 L190 5.95 Q190 2.25 186.'
+  + '5 2.05"/>'
+  + '    <path stroke="none" fill="#FFFFFF" d="M186.5 2.05 Q190 2.25 190 5.95 L190 58 Q190 62 186 62 L6 62 Q2 62 2 58 L2 5.95 Q2 2.3 '
+  + '5.4 2.05 L5.9 2 6 2 186.5 2.05 M186.4 4.05 L6.1 4 5.6 4.05 5.55 4.05 Q4 4.2 4 5.95 L4 58 Q4 60 6 60 L186 60 Q188 60 188 58 L188 '
+  + '5.95 Q188 4.2 186.4 4.05"/>'
+  + '    <path stroke="none" fill="#DADCE7" d="M186.4 4.05 Q188 4.2 188 5.95 L188 58 Q188 60 186 60 L6 60 Q4 60 4 58 L4 5.95 Q4 4.2 5'
+  + '.55 4.05 L5.6 4.05 6.1 4 186.4 4.05"/>'
+  + '  </g>'
+  + '</svg>'
+;
+SVGUIPack[CSVGButtonRectangleLine] := 
+  '<svg width="192" height="64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">'
+  + '  <defs/>'
+  + '  <g>'
+  + '    <path stroke="none" fill="#9696A3" d="M0 58.25 L0 5.95 Q0.05 0 6 0 L186 0 Q192 0 192 5.95 L192 58.25 Q191.85 64 186 64 L6 64'
+  + ' Q0.15 64 0 58.25 M186.5 2.05 L6 2 5.9 2 5.4 2.05 Q2 2.3 2 5.95 L2 58 Q2 62 6 62 L186 62 Q190 62 190 58 L190 5.95 Q190 2.25 186.'
+  + '5 2.05"/>'
+  + '    <path stroke="none" fill="#FFFFFF" d="M186.5 2.05 Q190 2.25 190 5.95 L190 58 Q190 62 186 62 L6 62 Q2 62 2 58 L2 5.95 Q2 2.3 '
+  + '5.4 2.05 L5.9 2 6 2 186.5 2.05 M186.4 4.05 L6.1 4 5.6 4.05 5.55 4.05 Q4 4.2 4 5.95 L4 58 Q4 60 6 60 L186 60 Q188 60 188 58 L188 '
+  + '5.95 Q188 4.2 186.4 4.05"/>'
+  + '    <path stroke="none" fill="#DADCE7" d="M186.4 4.05 Q188 4.2 188 5.95 L188 58 Q188 60 186 60 L6 60 Q4 60 4 58 L4 5.95 Q4 4.2 5'
+  + '.55 4.05 L5.6 4.05 6.1 4 186.4 4.05 M6.3 6 L6 6.05 6 58 186 58 186 6.05 6.3 6 M184 8.05 L184 56 8 56 8 8 184 8.05"/>'
+  + '    <path stroke="none" fill="#36BDF7" d="M184 8.05 L8 8 8 56 184 56 184 8.05 M6.3 6 L186 6.05 186 58 6 58 6 6.05 6.3 6"/>'
+  + '  </g>'
+  + '</svg>'
+;
+
+RegisterSVGBitmap;
 
 end.
